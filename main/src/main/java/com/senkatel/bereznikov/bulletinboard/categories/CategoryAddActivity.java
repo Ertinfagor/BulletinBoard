4522479c9754ee9e72f1396c4868d8f5ca09ae1b
Ertinfagor <abereznik@gmail.com>
2014-08-20 19:12:00 +0300

    Add icons

---
+++ main/src/main/java/com/senkatel/bereznikov/bulletinboard/categories/CategoryAddActivity.java
@@ -0,0 +1,7 @@
+package com.senkatel.bereznikov.bulletinboard.categories;
+
+/**
+ * Created by Bereznik on 20.08.2014.
+ */
+public class CategoryAddActivity {
+}
---
+++ main/src/main/java/com/senkatel/bereznikov/bulletinboard/cities/CityAddActivity.java
@@ -0,0 +1,7 @@
+package com.senkatel.bereznikov.bulletinboard.cities;
+
+/**
+ * Created by Bereznik on 20.08.2014.
+ */
+public class CityAddActivity {
+}
