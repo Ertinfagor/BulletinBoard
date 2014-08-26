e865c5dafe4c2304c6440b5b06b0793b32af6703
Ertinfagor <abereznik@gmail.com>
2014-08-26 12:50:19 +0300

    update apk

--- main/src/main/java/com/senkatel/bereznikov/bulletinboard/util/Constants.java
+++ main/src/main/java/com/senkatel/bereznikov/bulletinboard/util/Constants.java
@@ -6,15 +6,15 @@ package com.senkatel.bereznikov.bulletinboard.util;
  */
 public interface Constants {
 	public static final String LOG_TAG = "BulletinBoard";
-	public static final String URL = "http://195.66.136.204:3000";
-	//public static final String URL = "http://mdm.senkatel.com:3000";
+	//public static final String URL = "http://195.66.136.204:3000";
+	public static final String URL = "http://mdm.senkatel.com:3000";
 	public static final String BULLETIN = "/bulletin";
 	public static final String CATEGORY = "/category";
 	public static final String CITY = "/city";
 	public static final String CONTACT = "/contact";
 	public static final String IMAGE = "/image";
-	//public static final String DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'";
-	public static final String DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ";
+	public static final String DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'";
+	//public static final String DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ";
 	public static final int IMAGE_HEIHT = 100;
 	public static final int IMAGE_HEIHT_MAX = 1024;
 	public static final int IMAGE_WIDTH = 100;
